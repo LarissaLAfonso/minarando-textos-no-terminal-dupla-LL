9ba5cb428277591882f0f2ee3fd869f55fb7529b
LARISSA LEMOS AFONSO <B51055@fgv.edu.br>
2023-03-29 13:13:19 -0300

    exercício 1 - Larissa

---
+++ Solução/ex_1.sh
@@ -0,0 +1,57 @@
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado (main)
+$ ls
+CONTENTS  README  contos/  critica/  cronica/  miscelanea/  poesia/  romance/  teatro/  traducao/
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado (main)
+$ cd contos
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/contos (main)
+$ cat *.txt > intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/contos (main)
+$ cd ../critica
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/critica (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/critica (main)
+$ cd ../cronica
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/cronica (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/cronica (main)
+$ cd ../miscelanea
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/miscelanea (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/miscelanea (main)
+$ cd ../poesia
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/poesia (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/poesia (main)
+$ cd ../romance
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/romance (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/romance (main)
+$ cd ../teatro
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/teatro (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/teatro (main)
+$ cd ../traducao
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/traducao (main)
+$ cat *.txt >> intermediario.txt
+
+b51055@LAB-RJCC101 MINGW64 ~/mining/minarando-textos-no-terminal-dupla-LL/Dados/machado/traducao (main)
+$ wc intermediario.txt
+  28291  182137 1144431 intermediario.txt
+
+Logo, há 28291 linhas, 182137 palavras e 1144431 caracteres.
