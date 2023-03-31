47e62528a2eac1a2810aa943a9e51b3eb3d9a2b3
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-03-31 12:35:05 -0300

    Update ex_5.sh

--- Solução/ex_5.sh
+++ Solução/ex_5.sh
@@ -1,2 +1,2 @@
 cat arquivao.txt | sed ' /\n/g' > obra_linhas.txt
-tr '[:upper:]' '[:lower:]' < obra_linhas.txt | sed 's/[^a-z0-9 \n]//g' | sort | uniq -c | awk '{print $2, $1}' > reposta_ex_5.txt
+tr '[:upper:]' '[:lower:]' < obra_linhas.txt | sed 's/[^a-z0-9 \n]//g' | sort | uniq -c | awk '{print $2, $1}' > resposta_ex_5.txt
