5c2fe39ebdc944cabfab2c4f7058e4403260989f
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-03-31 08:53:13 -0300

    Update ex_4.sh

--- Solução/ex_4.sh
+++ Solução/ex_4.sh
@@ -1,3 +1,2 @@
 cat macn001.txt | sed ' /\n/g' > conto1_linhas.txt
 tr '[:upper:]' '[:lower:]' < conto1_linhas.txt | sed 's/[^a-z0-9 \n]//g' | sort | uniq -c | awk '{print $2, $1}' > reposta_ex_4.txt
-
