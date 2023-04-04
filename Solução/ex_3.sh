7b27efd86d3754378eed45778aa8601ebd9f894f
Larissa Lemos Afonso <126449248+LarissaLAfonso@users.noreply.github.com>
2023-04-03 21:35:10 -0300

    Update ex_3.sh

--- Solução/ex_3.sh
+++ Solução/ex_3.sh
@@ -1,4 +1,3 @@
-awk '{for(i=NF;i>=1;i--) printf("%s ",$i); printf("\n")}' linhas.txt > linhas_invertidas.txt
+awk '{for(i=NF;i>=1;i--) printf("%s ",$i); printf("\n")}' ../Dados/machado/linhas.txt > linhas_invertidas.txt
 sort linhas_invertidas.txt > linhas_ordenadas1.txt
-awk '{for(i=NF;i>=1;i--) printf("%s ",$i); printf("\n")}' linhas_ordenadas1.txt > linhas_ordenadas2.txt
-cat linhas_ordenadas2.txt > resposta_ex_3.txt
+awk '{for(i=NF;i>=1;i--) printf("%s ",$i); printf("\n")}' linhas_ordenadas1.txt > resposta_ex_2.txt
